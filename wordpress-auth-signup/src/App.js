6716b37008076fb7af8f12ddd11cdc0b5df0e42f
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-06-09 10:50:48 +0530

    Added jwt authentication

--- wordpress-auth-signup/src/App.js
+++ wordpress-auth-signup/src/App.js
@@ -1,12 +1,19 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import React, { Component } from 'react'
 import SignUp from './components/SignUp'
+import Login from './components/Login'
+import Home from './components/Home'
 
 export class App extends Component {
   render() {
     return (
-      <div className='App'>
-        <SignUp />
-      </div>
+      <BrowserRouter>
+        <div className='App'>
+            <Route exact path='/' component={Home} />
+            <Route path='/login' component={Login} />
+            <Route path='/signup' component={SignUp} />
+        </div>
+      </BrowserRouter>
     )
   }
 }
