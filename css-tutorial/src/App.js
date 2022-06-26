789ad08372d97f91973a70bfe4cab72f59235fba
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-06-26 17:41:00 +0530

    ⚡️ Global Theme using ThemeProvider

--- css-tutorial/src/App.js
+++ css-tutorial/src/App.js
@@ -1,14 +1,25 @@
+import { ThemeProvider } from 'styled-components';
 import Header from './components/Header';
 import { Container } from './components/styles/Container.styled';
 
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333'
+  }
+}
+
 function App() {
   return (
-    <>
-      <Header />
+    <ThemeProvider theme={theme}>
+      <>
+        <Header />
         <Container>
           <h1>Hello World!!</h1>
         </Container>
-    </>
+      </>
+    </ThemeProvider>
   );
 }
 
