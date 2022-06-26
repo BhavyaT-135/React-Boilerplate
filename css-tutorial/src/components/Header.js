c2877118ec97ce83f31123d6f4fe7a92114da090
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-06-26 17:22:59 +0530

    ⚡️ Basic Styled Components for Header

---
+++ css-tutorial/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyledHeader } from './styles/Header.styled'
+
+export default function Header() {
+  return (
+        <StyledHeader>
+            <h1>Hubble</h1>
+        </StyledHeader>
+  )
+}
---
+++ css-tutorial/src/components/styles/Container.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 1000px;
+    max-width: 100%;
+    paddin: 0 20px;
+    margin: 0 auto;
+`
+export default Container;
