be79d64d8999593f7853eaa1e0892cef89a7742b
BhavyaT-135 <bhavyatewari.13.5@gmail.com>
2022-07-26 23:03:58 +0530

    ⚡️ Deleting the sample testing folder

--- react-testing-starter-main/README.md
+++
@@ -1,3 +0,0 @@
-Thanks to team Cypress for this real world demo app ❤️
-
-<a href="https://github.com/cypress-io/cypress-realworld-app">Original repo</a>
--- react-testing-starter-main/src/utils/portUtils.ts
+++
@@ -1,4 +0,0 @@
-require("dotenv").config();
-
-export const frontendPort = process.env.REACT_APP_PORT;
-export const backendPort = process.env.REACT_APP_BACKEND_PORT;
